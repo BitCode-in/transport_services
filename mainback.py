523f695f8a7566eb4b0570598665e6610a66079e
Andreev <andreev0597@icloud.com>
2023-11-25 15:39:05 +0300

    test

--- mainback.py
+++ mainback.py
@@ -129,8 +129,4 @@ class DB:
         self.c.execute(f"DELETE auto WHERE id_auto = {id_auto}")
         self.conn.commit()
 
-    def test(self, id_auto):
-        self.c.execute(f"DELETE auto WHERE id_auto = {id_auto}")
-        self.conn.commit()
-
 
