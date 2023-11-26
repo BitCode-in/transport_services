e126c4596d94301c5fe022a1fe747850ff4fda22
BitCode-In <moltovai@yandex.ru>
2023-11-26 17:13:15 +0300

    new

--- mainfront.py
+++ mainfront.py
@@ -216,6 +216,7 @@ class App(QMainWindow, engine.Ui_widget):
 			self.list_executor[i[0]] = i
 			self.list_executor_widget[i[0]] = TablePrint(str(i[0]), str(i[1]), str(i[2]), str(i[3]))
 			self.executorui.verticalLayout_3.addWidget(self.list_executor_widget[i[0]])
+			
 
 	def open_customers(self):
 		self.customers = QMainWindow()
